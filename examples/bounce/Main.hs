41d2e8e7944aad8acd17565acdccfef990fd1aaa
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2014-01-05 17:08:22 +1100

    Bouncy with platform

--- examples/bounce/Main.hs
+++ examples/bounce/Main.hs
@@ -15,16 +15,23 @@ gravitationalConstant = -0.5
 creeperWidth, creeperHeight :: Float
 (_, (creeperWidth, creeperHeight)) = boundingBox (scale 0.5 0.5 creeperSprite)
 
-data World = World {creeperPos :: Point, creeperVelocity :: Float, platformPos :: Point }
+platformPos :: Point
+platformPos = (-200, -150)
+
+platformWidth, platformHeight :: Float
+(_, (platformWidth, platformHeight)) = boundingBox (scale 0.5 0.5 platformSprite)
+
+
+data World = World {creeperPos :: Point, creeperVelocity :: Float}
 
 initialWorld :: World
-initialWorld = World {creeperPos = (-200, 0), creeperVelocity = 0, platformPos = (-200, -150)}
+initialWorld = World {creeperPos = (-200, 0), creeperVelocity = 0}
 
-main = play (InWindow "Bouncy Creeper" (600, 400) (50, 50)) white 30 initialWorld draw handle [applyGravity]
+main = play (InWindow "Bouncy Creeper" (600, 400) (50, 50)) white 30 initialWorld draw handle [applyVelocity, applyGravity]
   where
-    draw (World {creeperPos = (x, y), platformPos = (platformX, platformY)}) 
+    draw (World {creeperPos = (x, y)}) 
       = pictures [ translate x y (scale 0.5 0.5 creeperSprite)
-                 , translate platformX platformY (scale 0.5 0.5 platformSprite)
+                 , translate (fst platformPos) (snd platformPos) (scale 0.5 0.5 platformSprite)
                  ]
     
     handle (EventKey (Char 'a') Down _ _)            world = world {creeperPos = moveX (creeperPos world) (-10)}
@@ -32,10 +39,20 @@ main = play (InWindow "Bouncy Creeper" (600, 400) (50, 50)) white 30 initialWorl
     handle (EventKey (SpecialKey KeySpace) Down _ _) world = world {creeperVelocity = 10}
     handle _event                         world            = world
     
-    applyGravity _ world@(World {creeperVelocity = velocity}) 
-      = world { creeperPos      = moveY (creeperPos world) velocity
-              , creeperVelocity = velocity + gravitationalConstant}
-
+    applyVelocity _ world
+      = world {creeperPos = moveY (creeperPos world) (creeperVelocity world)}
+    
+    applyGravity _ world
+      | onTheFloor world
+        || onThePlatform world = world {creeperVelocity = creeperVelocity world * (-0.5)}
+      | otherwise              = world {creeperVelocity = creeperVelocity world + gravitationalConstant}
+    
+    onTheFloor world = snd (creeperPos world) <= (-200) + creeperHeight / 2
+    
+    onThePlatform world = snd (creeperPos world) <= snd platformPos + creeperHeight / 2 + platformHeight / 2
+                          && fst (creeperPos world) + creeperWidth / 2 > fst platformPos - platformWidth / 2
+                          && fst (creeperPos world) - creeperWidth / 2 < fst platformPos + platformWidth / 2
+    
 moveX :: Point -> Float -> Point
 moveX (x, y) offset = if x + offset > 300 - creeperWidth / 2
                       then (300 - creeperWidth / 2, y)
@@ -48,4 +65,8 @@ moveY (x, y) offset = if y + offset > 200 - creeperHeight / 2
                       then (x, 200 - creeperHeight / 2)
                       else if y + offset < (-200) + creeperHeight / 2
                       then (x, (-200) + creeperHeight / 2)
+                      else if y < snd platformPos + creeperHeight / 2 + platformHeight / 2 
+                              && x + creeperWidth / 2 > fst platformPos - platformWidth / 2
+                              && x - creeperWidth / 2 < fst platformPos + platformWidth / 2
+                      then (x, snd platformPos + creeperHeight / 2 + platformHeight / 2)
                       else (x, y + offset)
